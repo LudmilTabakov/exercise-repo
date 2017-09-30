7efa26ffaa6d8f8a93d3911ed575c0ab59f1ff07
LudmilTabakov <32372415+LudmilTabakov@users.noreply.github.com>
2017-09-30 13:42:07 +0300

    Add files via upload

---
+++ StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub!");
+        }
+    }
+}
